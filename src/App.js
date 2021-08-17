3027d7a1db2ae37215046b01c974d01f4aeea6db
awesomecoder233 <tommount58@gmail.com>
2021-08-17 07:27:33 +0200

    define a validateForm function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ class App extends Component {
   }
 }
 const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (valid = false)
+  );
+  return valid;
+}
 
 class Register extends Component {
   constructor(props) {
